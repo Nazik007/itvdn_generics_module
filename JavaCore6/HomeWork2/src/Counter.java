d84b25cdd73d5e44fc026ea3ec264431d2a1de92
Nazarii Soviak <natesoviak@gmail.com>
2022-04-08 13:30:33 +0300

    ДЗ 2 Завданя 17

---
+++ JavaCore6/HomeWork2/src/Counter.java
@@ -0,0 +1,23 @@
+public class Counter {public long current(long number){
+    return number;
+}
+
+    public long next(long number){
+        return ++number;
+    }
+
+    public long prev(long number){
+        return --number;
+
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println("current");
+        System.out.println("next");
+        System.out.println("prev");
+
+
+    }
+}

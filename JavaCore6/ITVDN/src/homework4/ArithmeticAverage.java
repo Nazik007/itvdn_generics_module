ad0cda88114e0e57708ad538cd88ad0a003100bc
Nazarii Soviak <natesoviak@gmail.com>
2022-05-27 16:56:05 +0300

    For ITVDN

---
+++ JavaCore6/ITVDN/src/homework4/ArithmeticAverage.java
@@ -0,0 +1,12 @@
+package homework4;
+
+public class ArithmeticAverage {
+    public static void main(String[] args) {
+
+
+        double a = 37, b = 68, c = 31;
+        double d = (a + b + c) / 3;
+
+        System.out.println(d);
+    }
+}
